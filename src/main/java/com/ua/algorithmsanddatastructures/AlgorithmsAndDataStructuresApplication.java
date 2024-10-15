3eecc64f3c2c1a525b97b653759e63ca39489acf
BelozertsevVS <bvs.wl1980@gmail.com>
2024-10-15 14:05:43 +0300

    Lesson1

---
+++ src/main/java/com/ua/algorithmsanddatastructures/AlgorithmsAndDataStructuresApplication.java
@@ -0,0 +1,13 @@
+package com.ua.algorithmsanddatastructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsAndDataStructuresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsAndDataStructuresApplication.class, args);
+    }
+
+}
